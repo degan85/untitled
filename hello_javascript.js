7c6234f064475642cfd7f77b44e74b2df253326a
degan85 <degan8535@gmail.com>
2017-06-26 09:45:06 +0900

    이동

--- hello_javascript.js
+++ hello_javascript.js
@@ -4,7 +4,8 @@
 var val=0;
 var vals=[];
 var time=0;
-var pos=0;
+var pos_x=0;
+var pos_y=0;
 var animate_left = 0;
 var animate_top = 0;
 var str='';
@@ -24,24 +25,25 @@ $(document).ready(function(){
 
 
         time=0;
+        val = $('#input1').val();
+        vals = val.split(';');
 
         set_time(time);
 
     });
 });
 
-function set_time(time) {
-    alert(val.length);
-    alert(time+' 밖에');
-    val = $('#input1').val();
-    vals = val.split(';');
-    if(time < vals.length-1){
-        id = setInterval(frame, 20);
-        alert(time+' 안에');
-        var va = vals[time];
-        pos = $('#animate').offset().left;
 
-        set_values(id, va);
+
+function set_time(time) {
+    // alert("길이? : "+vals.length);
+    // alert(time+' 밖에');
+    if(time < vals.length-1){
+        // alert(time+' 안에');
+        var va = vals[time];
+        pos_x = $('#animate').offset().left;
+        pos_y = $('#animate').offset().top;
+        set_values(va);
     } else {
         alert('끝~');
         clearInterval(id);
@@ -49,13 +51,15 @@ function set_time(time) {
     }
 }
 
-function set_values(id, va) {
+function set_values(va) {
+    id = setInterval(frame, 20);
+
     va = va.replace(/ /g,"");
     num = va.replace(/[^0-9]/g,"");
     str = va.replace(/[0-9]/g,"");
     // alert('num : '+num+', str : '+str);
     if(str === 'left') {
-        pos = pos - 8;
+        pos_x = pos_x - 8;
     }
     frame();
 }
@@ -66,37 +70,72 @@ function frame() {
         frame_left_move();
     }else if('right'=== str){
         frame_right_move();
+    }else if('up'==str) {
+        frame_up_move();
+    }else if('down'==str) {
+        frame_down_move();
     }
 
 }
 
 function frame_left_move() {
 
-    if (pos <= (parseInt(animate_left)) - parseInt(num)) {
+    if (pos_x <= (parseInt(animate_left)) - parseInt(num)) {
         clearInterval(id);
-        time = time+1;
+        time++;
         animate_left = parseInt($('#animate').offset().left)-8;
-        set_time(id, time);
-    }else if (pos < 10) {
+        set_time(time);
+    }else if (pos_x < 10) {
         clearInterval(id);
     }else {
-        pos--;
-        elem.style.left = pos + 'px';
+        pos_x--;
+        elem.style.left = pos_x + 'px';
     }
 }
 
 
 function frame_right_move() {
 
-    if (pos >= (parseInt(animate_left))+ parseInt(num)) {
+    if (pos_x >= (parseInt(animate_left))+ parseInt(num)) {
         clearInterval(id);
-        time = time+1;
+        time++;
         animate_left = parseInt($('#animate').offset().left)-8;
         set_time(time);
-    }else if (pos > 350) {
+    }else if (pos_x > 350) {
         clearInterval(id);
     }else {
-        pos++;
-        elem.style.left = pos + 'px';
+        pos_x++;
+        elem.style.left = pos_x + 'px';
+    }
+}
+
+function frame_up_move() {
+
+    if (pos_y <= (parseInt(animate_top)) - parseInt(num)) {
+        clearInterval(id);
+        time++;
+        animate_top = parseInt($('#animate').offset().top);
+        set_time(time);
+    }else if (pos_y < 10) {
+        clearInterval(id);
+    }else {
+        pos_y--;
+        elem.style.top = (pos_y-94) + 'px';
+    }
+}
+
+
+function frame_down_move() {
+
+    if (pos_y >= (parseInt(animate_top))+ parseInt(num)) {
+        clearInterval(id);
+        time++;
+        animate_top = parseInt($('#animate').offset().top);
+        set_time(time);
+    }else if (pos_y > 350) {
+        clearInterval(id);
+    }else {
+        pos_y++;
+        elem.style.top = (pos_y-94) + 'px';
     }
 }
